4c20f0cf45e508ad64a7ea42d87058cb86c85213
Orlando Gordon <ogordon1998@gmail.com>
2023-07-18 02:02:16 -0400

    Add secure=true to the cookie options.

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,10 +20,10 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true,
     sameSite: 'none',
     // Domain: 'wander-xggp.onrender.com',
   };
-  //  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
 
   user.password = undefined;
